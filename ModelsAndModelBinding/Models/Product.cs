9d201508a859df6a7805f25a2fefdb362c524e3f
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2025-07-06 16:28:15 -0700

    Add product creation functionality and validation
    - Implemented Create action in ProductController for handling product creation via GET and POST requests.
    - Added data annotations for validation in the Product model, including length, required, range, and data type attributes.
    - Updated Index.cshtml to set the page title and add a header for the product list.

--- ModelsAndModelBinding/Models/Product.cs
+++ ModelsAndModelBinding/Models/Product.cs
@@ -1,4 +1,6 @@
-﻿namespace ModelsAndModelBinding.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelsAndModelBinding.Models;
 
 /// <summary>
 /// Represents an individual salable product
@@ -15,16 +17,20 @@ public class Product
     /// The consumer facing name of the product. May
     /// contain alphanumeric characters.
     /// </summary>
+    [StringLength(20)]
+    [Required(ErrorMessage = "Please provide a product name")]
     public required string Name { get; set; }
 
     /// <summary>
     /// The pre-tax sales price the customer must pay.
     /// </summary>
+    [Range(0.01, 10000)] // Most expensive item is 10k
     public double Price { get; set; }
 
     /// <summary>
     /// The date and time that the product became
     /// available for sale
     /// </summary>
+    [DataType(DataType.DateTime)]
     public DateTime StartSalesDate { get; set; }
 }
